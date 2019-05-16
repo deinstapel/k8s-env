906f7049c5f416dc95140ef3a2d80bc435eac2a3
Martin Koppehel <martin.koppehel@st.ovgu.de>
2019-05-16 15:56:16 +0200

    More robust handling and stopping tiller on exit

--- env.sh
+++ env.sh
@@ -12,12 +12,19 @@ export LOGFILE="${DIR}/__run-$(timestamp).log"
 export KUBECONFIG="${DIR}/kubeconfig"
 export HELM_HOME="${DIR}/helm-home"
 
-if [ -z "$1" ]; then
-  echo "You have to specify your username as first argument, e.g. martin"
+function cleanup {
+  echo -n 'Stopping tiller... '
+  helm tiller stop
+  unset DS_INIT
+  echo 'Done'
+}
 
+if [ "$DS_INIT" = "true" ]; then
+  echo "Environment already initialized, exiting"
+elif [ -z "$1" ]; then
+  echo "You have to specify your username as first argument, e.g. martin"
 else 
   echo 'Setting up helm'
-
   rm -rf "${HELM_HOME}" 2>/dev/null
   kubectl create ns $1-tiller 2>/dev/null || true >> "${LOGFILE}"
   helm init --client-only >> "${LOGFILE}"
@@ -25,8 +32,11 @@ else
   helm tiller start-ci $1-tiller >> "${LOGFILE}"
   source <(helm tiller env) 
 
+  export DS_INIT="true"
+  trap cleanup EXIT
+
   echo 'THIS CLUSTER IS LIVE'
   echo 'Running workloads:'
-  helm ls
+  helm ls || echo 'Failed to load releases'
 fi
-
+set +e
